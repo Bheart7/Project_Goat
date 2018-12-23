5398515c588dc5e0bec903d662211fe446b36655
Bheart Shrestha <bheartshrestha7@gmail.com>
2018-12-23 17:28:17 +0545

    offer page

---
+++ Project/animation.js
@@ -0,0 +1,47 @@
+
+   window.sr = ScrollReveal();
+   sr.reveal('.navbar',{
+     duration: 2000,
+     origin:'bottom'
+   })
+   sr.reveal('.showcase-left',{
+     duration: 2000,
+     origin:'top',
+     distance:'300px'
+   });
+   sr.reveal('.showcase-right',{
+     duration:2000,
+     origin:'right',
+     distance:'300px'
+   });
+   sr.reveal('.topcase-left',{
+     duration:2000,
+     origin:'left',
+     distance:'300px'
+   });
+   sr.reveal('.topcase-right',{
+     duration: 2000,
+     origin:'top',
+     distance:'300px'
+   });
+   sr.reveal('.btn',{
+     duration:2000,
+     delay:2000,
+     origin:'bottom'
+   });
+   sr.reveal('#testimonial div',{
+     duration:2000,
+     origin:'bottom',
+   });
+   sr.reveal('.info-left',{
+     duration:2000,
+     origin:'left',
+     distance:'300px',
+     viewFactor: 0.2
+   });
+   sr.reveal('.info-right',{
+     duration:2000,
+     origin:'right',
+     distance:'300px',
+     viewFactor: 0.2
+   });
