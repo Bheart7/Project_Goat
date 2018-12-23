1c5cee139be998361bbc313f733155f104a9d101
severus_bibash <38889169+acharyabibash@users.noreply.github.com>
2018-12-23 18:45:32 +0545

    Add files via upload

---
+++ imart/urls.py
@@ -0,0 +1,26 @@
+"""imart URL Configuration
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/1.11/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  url(r'^$', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  url(r'^$', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.conf.urls import url, include
+    2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
+"""
+from django.conf.urls import url
+from django.contrib import admin
+
+from .views import home_page, login_page, register_page
+
+urlpatterns = [
+	url(r'^$',home_page),
+    url(r'^admin/', admin.site.urls),
+	url(r'^login/',login_page),
+	url(r'^register/',register_page),    
+]
